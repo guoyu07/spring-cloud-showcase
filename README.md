151f175c987954db34cd5bb7e85c17de26d30964
judasn <jn3.141592654@gmail.com>
2017-10-18 23:27:21 +0800

    2017-10-18 初始化提交

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# spring-cloud-showcase
-spring-cloud-showcase
+# 说明
+
+
+## 项目创建顺序
+
+- spring-cloud-config
+- spring-cloud-registry
+
+
+## 参考项目
+
+- PiggyMetrics：<https://github.com/sqshq/PiggyMetrics>
---
+++ spring-cloud-registry/src/main/java/com/gitnavi/registry/SpringCloudRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.gitnavi.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRegistryApplication.class, args);
+	}
+}
