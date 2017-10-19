9cd286b47da2c201b63436076d1e4bda3531d3b3
judasn <jn3.141592654@gmail.com>
2017-10-19 23:07:17 +0800

    2017-10-19 补充 log4j2 配置

--- spring-cloud-config/src/main/java/com/gitnavi/config/SpringCloudConfigApplication.java
+++ spring-cloud-config/src/main/java/com/gitnavi/config/SpringCloudConfigApplication.java
@@ -1,10 +1,15 @@
 package com.gitnavi.config;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCloudConfigApplication {
+
+	private static final Logger logger = LogManager.getLogger();
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigApplication.class, args);
